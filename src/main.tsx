8262a0940e9d9d622f1cc697568d40edfdbce01b
mariuslohoho <mariuslo888@gmail.com>
2024-10-19 17:20:00 +0100

    removed stock market page temp

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,12 @@ import "./index.css";
 import ClassicShop from "./pages/ClassicShop";
 import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
-import StockMarket from "./pages/StockMarket";
+// import StockMarket from "./pages/StockMarket";
 
 const router = createHashRouter([
   { path: "/", element: <Home />, errorElement: <ErrorPage /> },
   { path: "/Classic-Shop", element: <ClassicShop /> },
-  { path: "/Stock-Market", element: <StockMarket /> },
+  // { path: "/Stock-Market", element: <StockMarket /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
